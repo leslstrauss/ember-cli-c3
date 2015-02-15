14c4026636733ee49831cb3522125ae02cd84605
Leslie Strauss <leslie.strauss@gmail.com>
2015-02-15 01:00:18 -0800

    Had to reinstal OS X, fresh files here.

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'explore-ember-cli',
+    modulePrefix: 'new-ember-cli',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
